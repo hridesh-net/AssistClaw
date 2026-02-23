87a7d6a8eadbebed2a4e44f89c94fc9b8c751f33
hridesh-net <hridyaash0811@gmail.com>
2026-02-23 15:40:49 -0500

    fix: unblock internal packages and add missing sqlite_vec_bridge.go

---
+++ internal/memory/sqlite_vec_bridge.go
@@ -0,0 +1,41 @@
+package memory
+
+/*
+#cgo CFLAGS: -I../../vendor/github.com/mattn/go-sqlite3 -I../../vendor/github.com/asg017/sqlite-vec-go-bindings/cgo -DSQLITE_CORE
+#cgo linux LDFLAGS: -lm
+#include "sqlite3-binding.h"
+#include "sqlite-vec.h"
+
+int assistclaw_sqlite3_vec_init(sqlite3 *db) {
+    return sqlite3_vec_init(db, NULL, NULL);
+}
+*/
+import "C"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+
+	"github.com/mattn/go-sqlite3"
+)
+
+// RegisterVec manually registers the sqlite-vec extension on a go-sqlite3 connection.
+func RegisterVec(conn *sqlite3.SQLiteConn) error {
+	// go-sqlite3 does not expose the raw *sqlite3 pointer in a clean way,
+	// but we can use reflection to get the "db" field if we really need to.
+	// HOWEVER, there is a better way: use the driver's ConnectHook.
+
+	// We use reflection to get the internal *C.sqlite3 from go-sqlite3.SQLiteConn
+	v := reflect.ValueOf(conn).Elem()
+	f := v.FieldByName("db")
+	if !f.IsValid() {
+		return fmt.Errorf("could not find db field in SQLiteConn")
+	}
+
+	db := (*C.sqlite3)(unsafe.Pointer(f.Pointer()))
+	rc := C.assistclaw_sqlite3_vec_init(db)
+	if rc != C.SQLITE_OK {
+		return fmt.Errorf("failed to register sqlite-vec: rc=%d", int(rc))
+	}
+	return nil
+}
